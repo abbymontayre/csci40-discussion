f362182c2cfbd5899161374c5c58f69b8f25f59c
Raphaelle Abby Montayre <raphaelle.abby.montayre@student.ateneo.edu>
2026-03-16 08:51:02 +0800

    secret key edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+os.environ.setdefault(
+    "SECRET_KEY", "django-insecure-=oq5t^n0@cz_zc!$f%gkb(2+8(3&g@6y@kk0p981gj7af@acvr")
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
